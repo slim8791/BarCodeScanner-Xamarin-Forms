ed9bc37f2c067007df18a037a80c6f694361bd14
slim8791 <slim.hammami.atj@outlook.fr>
2018-01-31 11:45:42 +0100

    Add project files.

---
+++ BarCodeScanner/BarCodeScanner/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarCodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void ScanCustomPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScannerPage());
+        }
+
+        private async void GenerateBarcode(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new QrCodePage());
+        }
+    }
+}
---
+++ BarCodeScanner/BarCodeScanner/ScannerPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+
+namespace BarCodeScanner
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScannerPage : ContentPage
+    {
+        public ScannerPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void ZXingScannerView_OnOnScanResult(Result result)
+        {
+
+            // Stop analysis until we navigate away so we don't keep reading barcodes
+            zxing.IsAnalyzing = false;
+
+            // Show an alert
+            await DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+            // Navigate away
+            await Navigation.PopAsync();
+        }
+
+        private void Overlay_OnFlashButtonClicked(Button sender, EventArgs e)
+        {
+            zxing.IsTorchOn = !zxing.IsTorchOn;
+
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+
+    }
+}
